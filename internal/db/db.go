57a85d152c190bc02d19055bf07902adfafa550c
chaaaeeee <mash1o1o1o1@gmail.com>
2024-07-05 20:56:54 +0700

    initial commit

---
+++ internal/db/db.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	dbConfig "github.com/chaaaeeee/internal/config/database"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func NewDatabase() (*Database, error) {
+	dsn := fmt.Sprintf(dbConfig.User, dbConfig.Password, dbConfig.Protocol, dbConfig.Path, dbConfig.Name)
+	db, err := sql.Open(dbConfig.Driver, dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Database{db: db}, nil
+}
+
+type Database struct {
+	db *sql.DB
+}
+
+func (db *Database) IsConnedted() bool {
+	return false
+}
+
+func (db *Database) Reconnect() {
+}
---
+++ internal/middleware/auth.go
@@ -0,0 +1,3 @@
+package middleware
+
+// simple auth using jwt take claims n stuffs
