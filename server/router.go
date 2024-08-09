0794abb589952f92b9679ff40a6a2561c36d08c6
goatyoda <mash1o1o1o1@gmail.com>
2024-08-09 13:40:12 +0700

    add handlers and implement middleware (dummy)

--- server/router.go
+++ server/router.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"fmt"
 	_ "github.com/chaaaeeee/sireng/docs"
 	trackerHandler "github.com/chaaaeeee/sireng/internal/tracker/handler"
 	userHandler "github.com/chaaaeeee/sireng/internal/user/handler"
@@ -15,20 +14,36 @@ func swaggerHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func initializeRoutes(mux *http.ServeMux, userHandler userHandler.UserHandler, trackerHandler trackerHandler.TrackerHandler, middleware middleware.Middleware) *http.ServeMux {
-	createStudySessionHandler := http.HandlerFunc(trackerHandler.CreateStudySessionHandler)
-	tes := middleware.Authenticate(createStudySessionHandler)
+	// declare handlers
+	createStudySessionHandlerFunc := http.HandlerFunc(trackerHandler.CreateStudySession)
+	endStudySessionHandlerFunc := http.HandlerFunc(trackerHandler.EndStudySession)
+	getStudySessionsHandlerFunc := http.HandlerFunc(trackerHandler.GetStudySessions)
+	getStudySessionsByUserIdHandlerFunc := http.HandlerFunc(trackerHandler.GetStudySessionsByUserId)
+	// set middleware
+	createStudySessionHandler := middleware.Authenticate(createStudySessionHandlerFunc)
+	createStudySessionHandler = middleware.IsUser(createStudySessionHandler)
+
+	endStudySessionHandler := middleware.Authenticate(endStudySessionHandlerFunc)
+	endStudySessionHandler = middleware.IsUser(endStudySessionHandler)
+
+	getStudySessionsHandler := middleware.Authenticate(getStudySessionsHandlerFunc)
+	getStudySessionsHandler = middleware.IsUser(getStudySessionsHandler)
+
+	getStudySessionsByUserIdHandler := middleware.Authenticate(getStudySessionsByUserIdHandlerFunc)
+	getStudySessionsByUserIdHandler = middleware.IsUser(getStudySessionsByUserIdHandler)
+
+	// initialize handlers
 	mux.HandleFunc("POST /signup", userHandler.SignUp)
 	mux.HandleFunc("POST /login", userHandler.Login)
-	mux.HandleFunc("POST /createStudySession", trackerHandler.CreateStudySessionHandler)
-	mux.HandleFunc("POST /endStudySession", trackerHandler.EndStudySessionHandler)
 	// mux.HandleFunc("GET /logout", userHandler.Logout)
-	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("tes")
-	})
 
-	mux.HandleFunc("GET /swagger/*", swaggerHandler)
+	mux.Handle("POST /createStudySession", createStudySessionHandler)
+	mux.Handle("GET /endStudySession/{userId}", endStudySessionHandler)
+	mux.Handle("GET /getStudySessions", getStudySessionsHandler)
+	mux.Handle("GET /getStudySessions/{userId}", getStudySessionsByUserIdHandler)
 
-	mux.Handle("POST /middlewareTest", tes)
+	// nantian
+	// mux.HandleFunc("GET /swagger/*", swaggerHandler)
 
 	return mux
 }
